2dbda258ad62dab1bdfea695dba4cc4c3436cfff
Burris19 <julianhernandez19@gmail.com>
2016-03-31 00:18:43 -0600

    [add] unlik events to elements with jquery

--- capitulo2/js/capitulo2.js
+++ capitulo2/js/capitulo2.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    ejemplo2_1();
+    // ejemplo2_1();
+    ejemplo2_2();
 });
 
 
@@ -13,9 +14,20 @@ function ejemplo2_1(){
 
  	$('#ejemplo1').click(function(){
  		objeto.trigger('alert', 'Pasando parametros de evento', $(this));  
- 	});
+ 	}); 	
+}
 
- 	
+function ejemplo2_2(){
+	var objeto = {};
+	_.extend(objeto, Backbone.Events);
+	objeto.on('alert', function(msg, extend){
+		extend.append("<div class='tested'>Ok</div>");
+		objeto.off('alert');
+	});
+
+	$('#ejemplo1').click(function(){
+		objeto.trigger('alert', 'Pasando parametros de evento', $(this));
+	});
 }
 
 
