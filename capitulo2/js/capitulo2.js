aa94203a88440c84cf1b3f4fec4bd3831d3917b7
Burris19 <julianhernandez19@gmail.com>
2016-03-31 00:11:34 -0600

    [add] assign events to elements with jquery

--- capitulo2/js/capitulo2.js
+++ capitulo2/js/capitulo2.js
@@ -4,7 +4,18 @@ $(document).ready(function(){
 
 
 function ejemplo2_1(){
-   
+ 	var objeto = {};
+ 	_.extend(objeto, Backbone.Events);
+ 	objeto.on('alert', function(msg, e){
+ 		alert(msg);
+ 		e.append("<div class='tested' >Ok</div>");
+ 	});
+
+ 	$('#ejemplo1').click(function(){
+ 		objeto.trigger('alert', 'Pasando parametros de evento', $(this));  
+ 	});
+
+ 	
 }
 
 
