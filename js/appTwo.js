995222828c4e69c1c1de77642bb2fa120785d996
Burris19 <julianhernandez19@gmail.com>
2016-04-11 01:35:20 +0000

    [add] creando colecciones

--- js/appTwo.js
+++ js/appTwo.js
@@ -1,5 +1,7 @@
+var paneles;
+
 $(function(){
-	$("#create_button").click(function(){
+	// $("#create_button").click(function(){
 		// var panel1 = new Panel();
 		// var panel2 = new Panel();
 
@@ -11,10 +13,10 @@ $(function(){
 		// window.trace(panel2.propInstancia + ' ' + Panel.propEstatica);
 
 		// var panel1 = new Panel();
-		var panel = new Panel({
-			txt: "Este es el nuevo panel",
-			rotulo: "Mi panel"
-		});
+		// var panel = new Panel({
+		// 	txt: "Este es el nuevo panel",
+		// 	rotulo: "Mi panel"
+		// });
 
 		// window.trace(panel.get("rotulo"));
 		// window.trace(panel.toString());
@@ -22,18 +24,51 @@ $(function(){
 		// panel.set("isActive", "sdf");
 		// panel.isValid();
 
-		var subpanel = new SubPanel({
-			txt: "Este es el nuevo panel",
-			rotulo: "Mi panel"
-		});
-		
-		trace(subpanel.toString());
+		// var subpanel = new SubPanel({
+		// 	txt: "Este es el nuevo panel",
+		// 	rotulo: "Mi panel"
+		// });
+
+		// trace(subpanel.toString());
 		//window.trace(JSON.stringify(panel2.toJSON()));
 		// console.log(panel1);
 		// console.log(panel2);
 
+
+
+	// });
+
+	var PanelesCollection = Backbone.Collection.extend({
+		model: Panel
 	});
+
+	paneles = new PanelesCollection([
+		{txt: "Este es el panel1", rotulo: "Panel1", id:"1"},
+		{txt: "Este es el panel2", rotulo: "Panel1", id:"2"},
+		{txt: "Este es el panel3", rotulo: "Panel1", id:"3"}
+	]);
+
+	trace(JSON.stringify(paneles.toJSON()))
+
+
 });
+
+function pintaPanel(data){
+	var $div = $("<li>", {id: "ref_panel_"+data.cid});
+
+	$div.html(data.get("rotulo"));
+	$div.data("id_panel", data.cid);
+	$div.click(function(){
+		$(this).toggleClass("panel_seleccionado");
+		if($.seleccionado != undefined && $.seleccionado.data("id_panel") != $(this).data("id_panel")){
+			$.seleccionado.toggleClass("panel_seleccionado");
+		}
+		$.seleccionado = $(this);
+	});
+	$("#listado ul").append($div);
+}
+
+
 
 //Funcion que escribe un mensaje en la consola
 
--- js/models/Panel.js
+++ js/models/Panel.js
@@ -1,41 +1,52 @@
 var Panel = Backbone.Model.extend(
 	{
-		prefijo: "Texto del panel:",
+		// prefijo: "Texto del panel:",
 		defaults: {
 			txt: "Aun no se ha defino texto",
 			rotulo:"Sin nombre",
-			isActive: false
+			// isActive: false
 		},
 		initialize: function(attr){
-			this.on("change", this.onChnageAll, this);
-			this.on("change:rotulo", this.onChnageRotulo, this);
-			this.on("invalid", this.onInvalid, this);
-			// window.trace("El text es " + attr.txt);
-			this.set("txt", this.prefijo + attr.txt);
+			// this.on("change", this.onChnageAll, this);
+			// this.on("change:rotulo", this.onChnageRotulo, this);
+			// this.on("invalid", this.onInvalid, this);
+			this.on("change:txt", this.onChangeTxt, this);
+			window.trace("Creando un nuevo panel con texto:  " + this.get("txt"));
+			// this.set("txt", this.prefijo + attr.txt);
 		},
-		toString: function(){
-			return "El texto es: " + this.get("txt");
+		promptTexto: function(){
+			var _txt = prompt("Escribe el texto que quieres agregar:");
+			this.set({txt: _txt});
 		},
-		onChnageAll: function(){
-			trace("Se han cambiado propiedades del modelo Panel");
-		},
-		onChnageRotulo: function(model, options){
-			trace("Se ha cambiado el rotulo por " + this.get("rotulo"));
-		},
-		validate: function(attrs, options){
-			if(attrs.isActive != undefined && !_.isBoolean(attrs.isActive)){
-				return "El valor de activacion no es correcto";
-			}
-		},
-		onInvalid: function(model, error){
-			alert(error);			
+		onChangeTxt: function(model, options){
+			trace("el texto ha sigo cambiado por: " + model.changed.txt);
 		}
+		// toString: function(){
+		// 	return "El texto es: " + this.get("txt");
+		// },
+		// onChnageAll: function(){
+		// 	trace("Se han cambiado propiedades del modelo Panel");
+		// },
+		// onChnageRotulo: function(model, options){
+		// 	trace("Se ha cambiado el rotulo por " + this.get("rotulo"));
+		// },
+		// validate: function(attrs, options){
+		// 	if(attrs.isActive != undefined && !_.isBoolean(attrs.isActive)){
+		// 		return "El valor de activacion no es correcto";
+		// 	}
+		// },
+		// onInvalid: function(model, error){
+		// 	alert(error);			
+		// }
+	},{
+		rotulo_init: "Panel con rotulo ",	
 	}
+	
 );
 
 
-var SubPanel = Panel.extend({
-	{
-		prefijo: "Texto del SUBPANEL",
-	}
-});
+// var SubPanel = Panel.extend({
+// 	{
+// 		prefijo: "Texto del SUBPANEL",
+// 	}
+// });
